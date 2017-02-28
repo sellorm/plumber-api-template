66e5ea968d729cbaec4bbfd8a1e04f56947759b6
Mark Sellors <github@5vcc.com>
2017-02-28 01:15:18 +0000

    updated comment

--- plumber-api-template.R
+++ plumber-api-template.R
@@ -1,5 +1,7 @@
 # Plumber API template
 
+# in much the same way as it is for the rest of R, the docs are shipped with the code.
+
 #* @get /api/endpoint
 function(){
   return("Your function goes here")
